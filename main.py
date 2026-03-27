e7ec6c873f2eb16f8167c9335d759127d04e284a
zyan-007 <zyanprogramming@gmail.com>
2026-03-27 19:47:16 +0530

    feat: add calculator basic input function

---
+++ main.py
@@ -0,0 +1,21 @@
+def main():
+    user_input1 = int(input("num1: "))
+    user_input2 = int(input("num2: "))
+
+    user_option = chr(input("symbol(+/-/*): "))
+
+    print("answer: ", end="")
+
+    if user_option == '+':
+        ...
+    elif user_option == '-':
+        ...
+    elif user_option == "*":
+        ...
+    else:
+        print("Invalid option")
+
+    
+
+
+main()
