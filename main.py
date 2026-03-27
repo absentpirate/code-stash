8f7c28c17dfc09504642bb520edf4d90a7620fe3
zyan-007 <zyanprogramming@gmail.com>
2026-03-27 19:51:02 +0530

    feat: add subtract function

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ def main():
     user_input1 = int(input("num1: "))
     user_input2 = int(input("num2: "))
 
-    user_option = chr(input("symbol(+/-/*): "))
+    user_option = input("symbol(+/-/*): ")
 
     print("answer: ", end="")
 
     if user_option == '+':
         ...
     elif user_option == '-':
-        ...
+        print(subtract(user_input1, user_input2))
     elif user_option == "*":
         ...
     else:
         print("Invalid option")
 
-    
+def subtract(num1, num2):
+    return num1-num2
 
 
 main()
